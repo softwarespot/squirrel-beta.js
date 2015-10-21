b9d4efc7b7de3a95a0eba14b6229833b0da561de
softwarespot <softwarespotapps@gmail.com>
2015-10-21 20:43:40 +0300

    Removed extra whitespace

--- jquery.squirrel.js
+++ jquery.squirrel.js
@@ -376,10 +376,9 @@
     // onload.
     $(function () {
 
-        // load all forms that have the squirrel class  or data-squirrel attribute associated with them.
+        // load all forms that have the squirrel class or data-squirrel attribute associated with them.
         $('form.squirrel, form[data-squirrel]').squirrel();
 
     });
 
 })(this, this.jQuery);
-
